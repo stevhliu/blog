b41abfe3fa451711ebfc51f875dd311d5f96919d
Steven Liu <steven.liu@huggingface.co>
2024-07-01 14:55:01 -0700

    date

--- app/redis.ts
+++ app/redis.ts
@@ -1,12 +1,12 @@
 import { Redis } from "@upstash/redis";
 
-// if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
-//   throw new Error("UPSTASH_REDIS_REST_TOKEN is not defined");
-// }
+if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
+  throw new Error("UPSTASH_REDIS_REST_TOKEN is not defined");
+}
 
 const redis = new Redis({
   url: "https://usw1-native-pup-33102.upstash.io",
-  token: "AYFOASQgOTRiNWU2YTQtYThiZi00OGE4LTkwZTQtNGRhYTUyYWQ4MmJkYzczYjYwNmUxZGEyNGI4YTg4YTdjOWIwZTRjNDhlNmQ=",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default redis;
