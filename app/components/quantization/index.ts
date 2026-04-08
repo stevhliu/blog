ebcbb7fa7df00936ee2699562fbf53bb0117cd52
stevhliu <steven.liu@huggingface.co>
2026-04-07 21:26:32 -0700

    clean up

--- app/components/quantization/index.ts
+++
@@ -1,8 +0,0 @@
-export { AsymmetricQuantizationDiagram } from "./asymmetric-quantization-diagram";
-export { TensorParallelismDiagram } from "./tensor-parallelism-diagram";
-export { DeviceMapDiagram } from "./device-map-diagram";
-export { BMIDiagram } from "./bmi-diagram";
-export { FP32Diagram } from "./fp32-diagram";
-export { FP16Diagram } from "./fp16-diagram";
-export { Int8Diagram } from "./int8-diagram";
-export { KVCacheDiagram } from "./kv-cache-diagram";
--- lib/shikiHighlight.ts
+++
@@ -1,9 +0,0 @@
-import { codeToHtml } from 'shiki';
-
-export async function highlightCode(
-  code: string,
-  lang: string = 'python',
-  theme: string = 'catppuccin-mocha'
-) {
-  return await codeToHtml(code, { lang, theme });
-} 
