85905ff41f7dfd3dc08de02011f4c74968a4d363
stevhliu <steven.liu@huggingface.co>
2026-05-05 12:27:21 -0700

    clean

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const secretKey = process.env.SUPABASE_SECRET_KEY;
 
 export const supabase =
-  supabaseUrl && serviceRoleKey
-    ? createClient(supabaseUrl, serviceRoleKey, {
+  supabaseUrl && secretKey
+    ? createClient(supabaseUrl, secretKey, {
         auth: { autoRefreshToken: false, persistSession: false },
       })
     : null;
